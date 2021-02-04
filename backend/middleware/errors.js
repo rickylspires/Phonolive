9280878e6da7d8be71e273b3d3e2b16bdb3385bb
rickylspires <rickyspires@gmail.com>
2021-02-04 17:48:05 +0000

    Add Error Handeling - Mongodb Error - Async Errors, Unhandled promise errors - Uncourt errors - Mongo ID errors

---
+++ backend/middleware/errors.js
@@ -0,0 +1,38 @@
+const ErrorHandler = require('../utils/errorHandler');
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+
+    if(process.env.NODE_ENV === 'DEVELOPMENT'){
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack: err.stack
+        })
+    }
+
+    if(process.env.NODE_ENV === 'PRODUCTION'){
+        
+        let error = {...err}
+
+        error.message = err.message
+
+        // wrong Mongoose object ID error handling
+        if(err.name === 'castError'){
+            const message = `Resource not found. Invalid: ${err.path}`
+            error = new ErrorHandler(message, 400);
+        }
+
+        // wrong Mongoose object Validation handling
+        if(err.name === 'validationError'){
+            const message = object.values(err.errors).map(value => values.message);
+            error = new ErrorHandler(message, 400);
+        }
+        
+        res.status(error.statusCode).json({
+            success: false,
+            message: error.message || 'internal Server Error'
+        })
+    }
+}
