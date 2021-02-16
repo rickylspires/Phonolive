7bc12a85beebed7a1935c13365d9f2b8e32d468b
rickylspires <rickyspires@gmail.com>
2021-02-16 17:16:07 +0000

    Create the reset password model

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
-
+const crypto = require('crypto');
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -96,6 +96,40 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     }
 
 })
+
+// Reset Password   =>  /api/v1/password/reset
+// Reset Password   =>  /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    // Setup new password
+    user.password = req.body.password;
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res)
+
+})
+
+
 
 
 // Logout user = /api/v1/logout/
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,13 +5,17 @@ const {
     registerUser,
     loginUser,
     logout,
-    forgotPassword
+    forgotPassword, 
+    resetPassword
 } = require('../controllers/authController');
 
 //Public
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/logout').get(logout);
+
 router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
+
+router.route('/logout').get(logout);
 
 module.exports = router;
