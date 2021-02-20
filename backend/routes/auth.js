7a8839c72e95cf0bd2962e2c15bbccce140e8234
rickylspires <rickyspires@gmail.com>
2021-02-20 17:38:55 +0000

    Add Admin methods for get all, get user by id, update, delete users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,10 +7,16 @@ const {
     logout,
     forgotPassword, 
     resetPassword,
-    getUserProfile
+    getUserProfile,
+    updatePassword,
+    updateProfile,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
 } = require('../controllers/authController');
 
-const { isAuthenticatedUser } = require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles} = require('../middleware/auth');
 
 //Public
 router.route('/register').post(registerUser);
@@ -22,5 +28,14 @@ router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(logout);
 
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+
+//ADMIN - USERS
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id')
+.get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+.put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+.delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
 
 module.exports = router;
