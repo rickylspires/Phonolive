d420d2d530e4d09fd604fa85dc9252af2c223db9
rickylspires <rickyspires@gmail.com>
2021-02-24 20:56:23 +0000

    Add Home component with single static product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/layout/Header';
+import Home from './components/Home';
 import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
