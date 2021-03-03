e1ce5c58677cadb54e8d3d39b72ab0c3b382ff75
rickylspires <rickyspires@gmail.com>
2021-03-03 23:16:23 +0000

    Add product descriptio page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/layout/Header';
-import Home from './components/Home';
 import Footer from './components/layout/Footer';
+
+import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Header />
         <div className="container container-fluid">
           <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={ProductDetails} exact />
         </div>
         <Footer />
       </div>
