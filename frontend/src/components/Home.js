4b065c79844c8ffb5087b07cd4c0e0f40e12e72b
rickylspires <rickyspires@gmail.com>
2021-03-05 09:38:53 +0000

    add a slider filter to the search page + link logos to home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,7 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Pagination from 'react-js-pagination'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css';
 
 import MetaData from './layout/MetaData'
 import Product from './product/Product'
@@ -9,6 +11,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useAlert } from 'react-alert'
 import { getProducts } from '../actions/productActions'
 
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range)
 
 const Home = ({ match }) => {
 
@@ -16,42 +20,83 @@ const Home = ({ match }) => {
     const dispatch = useDispatch();
 
     const [currentPage, setCurrentPage] = useState(1)
-    const { loading, products, error, productsCount, resPerPage } = useSelector(state => state.products)
+    const [price, setPrice] = useState([1, 1000])
+
+    const { loading, products, error, productsCount, resPerPage, filteredProductsCount } = useSelector(state => state.products)
 
     const keyword = match.params.keyword
 
     useEffect(() => {
-
-        if(error) {
-            //alert.success('Success')
+        if (error) {
             return alert.error(error)
         }
 
-        dispatch(getProducts(keyword, currentPage));
+        dispatch(getProducts(keyword, currentPage, price));
 
-    }, [dispatch, alert, error, currentPage])
+    }, [dispatch, alert, error, keyword, currentPage, price])
 
     function setCurrentPageNo(pageNumber) {
         setCurrentPage(pageNumber)
     }
     
-    return (
-        <Fragment> 
+    let count = productsCount;
+    if (keyword) {
+        count = filteredProductsCount
+    }
 
+    return (
+        <Fragment>
             {loading ? <Loader /> : (
-                <Fragment> 
+                <Fragment>
                     <MetaData title={'Buy Best Products Online'} />
+
                     <h1 id="products_heading">Latest Products</h1>
+
                     <section id="products" className="container mt-5">
                         <div className="row">
-                            {products && products.map(product => (
-                                <Product key={product._id} product={product} />
-                            ))}
+
+                            {keyword ? (
+                                <Fragment>
+                                    <div className="col-6 col-md-3 mt-5 mb-5">
+                                        <div className="px-5">
+                                            <Range
+                                                marks={{
+                                                    1: `£1`,
+                                                    1000: `£1000`
+                                                }}
+                                                min={1}
+                                                max={1000}
+                                                defaultValue={[1, 1000]}
+                                                tipFormatter={value => `£${value}`}
+                                                tipProps={{
+                                                    placement: "top",
+                                                    visible: true
+                                                }}
+                                                value={price}
+                                                onChange={price => setPrice(price)}
+                                            />
+
+                                        </div>
+                                    </div>
+
+                                    <div className="col-6 col-md-9">
+                                        <div className="row">
+                                            {products.map(product => (
+                                                <Product key={product._id} product={product} col={4} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Fragment>
+                            ) : (
+                                    products.map(product => (
+                                        <Product key={product._id} product={product} col={3} />
+                                    ))
+                                )}
+
                         </div>
                     </section>
 
-
-                    {resPerPage <= productsCount && (
+                    {resPerPage <= count && (
                         <div className="d-flex justify-content-center mt-5">
                             <Pagination
                                 activePage={currentPage}
@@ -67,9 +112,11 @@ const Home = ({ match }) => {
                             />
                         </div>
                     )}
+    
 
                 </Fragment>
             )}
+
         </Fragment>
     )
 }
